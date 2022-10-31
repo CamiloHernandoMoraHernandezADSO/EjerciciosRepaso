87a8c3febbb854d0587081bff42d68a60be558fc
Camilo Mora <cm.prog0@gmail.com>
2022-10-30 21:52:21 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    let n1 = document.querySelector("#n1")
-    let resultado = ""
-    let formNum = document.querySelector("#formNumeros")
-    let suma = 0
-    let result = document.querySelector("#resultados")
-    formNum.addEventListener("submit", (e)=>{
+    let formVentas = document.querySelector("#formVentas")
+    let resultados = document.querySelector("#resultados")
+    let venta1 = document.querySelector("#venta1")
+    let venta2 = document.querySelector("#venta2")
+    let venta3 = document.querySelector("#venta3")
+    let sldBase = 1000000
+
+    formVentas.addEventListener("submit", (e)=>{
         e.preventDefault()
         let datosEntrada = Object.fromEntries(new FormData(e.target));
+        let com1 = datosEntrada.venta1*0.10
+        let com2 = datosEntrada.venta2*0.10
+        let com3 = datosEntrada.venta3*0.10
+        let totalComi = com1 + com2 + com3 
+        let sldTotal = totalComi + sldBase
+        resultados.innerHTML = ""
+        resultados.insertAdjacentHTML("beforeend",`
+        <h2>Total de la comisión por ventas: ${totalComi}</h2>
+        <h2>Sueldo total: ${sldTotal}</h2>
+        `);
 
-        for (let i = 1; i < datosEntrada.n1; i++) {
-           if(datosEntrada.n1 % i == 0){
-            suma += i
-           }
-            
-        }
-        if(datosEntrada.n1 == suma){
-            resultado = "Es perfecto"
-            suma = 0
-        }
-        else{
-            resultado = "NO es perfecto"
-            suma = 0
-        }
+        venta1.value = ""
+        venta2.value = ""
+        venta3.value = ""
 
-        result.innerHTML = ""
-        result.insertAdjacentHTML("beforeend",`
-                <h2> ${datosEntrada.n1 + ' ' + resultado}  </h2>
-                `);
     })
 })
